943fc92afb2780f38897129c0a9236aa24eb1e8b
Dhruv Kumar Sinha <2217dks@gmail.com>
2025-12-06 03:31:48 +0530

    Added Functionality and Improved Responsive Design

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('./pages/navbar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(htmlContent => {
-            const placeholder = document.getElementById('navbar-placeholder');
-            if (placeholder) {
-                placeholder.innerHTML = htmlContent;
-            }
-        })
-        .catch(error => {
-            console.error('Failed to load the universal navbar:', error);
-        });
-    fetch('./pages/footer.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(htmlContent => {
-            const placeholder = document.getElementById('footer-placeholder');
-            if (placeholder) {
-                placeholder.innerHTML = htmlContent;
-            }
-        })
-        .catch(error => {
-            console.error('Failed to load the font:', error);
-        });
-    fetch('./pages/font.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(htmlContent => {
-            const placeholder = document.getElementById('font-placeholder');
-            if (placeholder) {
-                placeholder.innerHTML = htmlContent;
-            }
-        })
-        .catch(error => {
-            console.error('Failed to load the font:', error);
-        });
-});
+let signBackBtn = document.getElementById('signBackBtn')
+let signBtn = document.getElementById('signBtn')
+let phone = document.getElementById('phone')
+let calc = document.getElementById('calc')
+let famCount = document.getElementById('family')
+let output = document.getElementById('recommendation')
+if (calc !== null){
+    calc.addEventListener('click', function(){
+        family = famCount.value
+        if (family <= 2) {
+            output.innerHTML = "You should get <b>StartWell Basic</b>"
+        } else if (family <= 4) {
+            output.innerHTML = "You should get <b>Family Care Plan</b>"
+        } else {
+            output.innerHTML = "You should get <b>Complete Balance Care</b>"
+        }
+    })
+}
+if (signBackBtn !== null){
+    signBackBtn.addEventListener('click', function(){
+        window.history.back()
+    })
+}
+if (signBtn !== null){
+    signBtn.addEventListener('click', function(){
+        alert(phone.value)
+    })
+}
